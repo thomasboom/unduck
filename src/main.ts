e16db463e6a69d1ccc5305c97782de0ed43034e4
ThomasNow Productions <thomasnowprod@proton.me>
2026-01-20 15:29:09 +0100

    Update icon

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,8 @@ function renderMainPage() {
         <header class="search-header">
           <div class="logo">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
-              <polyline points="10 17 15 12 10 7"></polyline>
-              <line x1="15" y1="12" x2="3" y2="12"></line>
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
             </svg>
           </div>
           <h1 class="title">Unduck</h1>
